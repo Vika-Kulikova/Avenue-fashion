0f2e79ea520a7c5a27bb3aefb4b9068aa943a913
Vika-Kulikova <visiabez@gmail.com>
2018-06-12 10:46:18 +0300

    Initial commit

---
+++ src/js/common.js
@@ -0,0 +1,16 @@
+$(function () {
+  // styler
+  $('select').styler();
+
+  // Masonry
+  var $grid = $('.grid').masonry({
+    itemSelector: '.grid-item',
+    percentPosition: true
+  });
+// layout Masonry after each image loads
+  $grid.imagesLoaded().progress(function () {
+    $grid.masonry('layout');
+  });
+
+
+});
